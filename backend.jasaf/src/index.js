3aacbf32ccddf75fdef043ac02e723ff64038093
Jessica Garces <jessygarcesliz@gmail.com>
2021-10-10 08:13:44 -0500

    Parte 3 del backend con el modulo de productos

--- backend.jasaf/src/index.js
+++ backend.jasaf/src/index.js
@@ -2,16 +2,23 @@
  * Index.js : File para realizar colocar las configuraciones
  * necesarias para la correcta ejecución de el app
  */
-
 const express = require("express");
+const cors = require("cors");
+const configs = require('./configs/config');
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.set('port', configs.server.port);
+
 require('./database/mongoFactory');
 
 //importamos las rutas definidas para el modulo productos 
 //y se incluyeron a express
-app.use(require('./routes/productos/productos'));
+app.use(require('./routes/productos/products.routes'));
 
 // Realiza la configuracion del puerto del servidor express
-app.listen(8080, () => {
-  console.log("Express server running on 6060");
+app.listen(app.get('port'), () => {
+  console.log("Express server running " + app.get('port'));
 });
