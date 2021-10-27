e7c5bea2c0863eb013784073322db348f6e5dc30
Fidel Fuerte <fidelfuerte90@gmail.com>
2021-10-27 11:25:59 -0500

    se realiza conexu¿ion a mongo Atlas

--- backend.jasaf/src/database/mongoFactory.js
+++ backend.jasaf/src/database/mongoFactory.js
@@ -5,10 +5,13 @@ const mongoose = require('mongoose');
 const config = require('../configs/config')
 
 const mongoUrl = config.database.protocol + '://' + 
+                 config.database.user + ':' +
+                 config.database.pass + '@'+ 
                  config.database.server + ':' + 
                  config.database.port + '/' +
-                 config.database.name;
+                 config.database.name + '?' +
+                 config.database.other
 
-mongoose.connect(mongoUrl)
+mongoose.connect("mongodb+srv://root:root@cluster0.e0v2h.mongodb.net/jasaf?retryWrites=true&w=majority")
         .then(db => console.log("mongodb connected"))
         .catch(err => console.error(err));
